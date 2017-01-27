80a1e0acf9c09e33b6e5260bde7ca1c2f0e49796
tomcat <risc604@ms40.url.com.tw>
2017-01-28 00:35:31 +0800

    create new project for java SQLite example code

---
+++ src/SQLiteJDBC.java
@@ -0,0 +1,131 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.sqlite.SQLiteConfig;
+import org.sqlite.SQLiteDataSource;
+import org.sqlite.SQLiteException;
+
+public class SQLiteJDBC 
+{
+	//private final String DBFILE = "jdbc:sqlite:data/mt24hr.db";
+	private final String DBFILE = "jdbc:sqlite:data/test.db";
+	
+	public SQLiteJDBC() throws SQLException
+	{
+		Connection con = getConnection();
+		
+		//建立 Table
+		createTable(con);
+		
+		//新增資料
+		insert(con, 1, "第一個");
+		insert(con, 2, "第二個");
+		//查詢顯示資料
+		System.out.println("新增第二筆資料後狀況:");	
+		selectAll(con);
+		
+		//修改資料
+		System.out.println("修改第一筆資料後狀況:");
+		update(con, 1, "這個植被給變了!");
+		//查詢顯示資料
+		selectAll(con);
+		
+		//刪除資料
+		System.out.println("刪除第一筆資料後狀況:");
+		delete(con, 1);
+		//查詢顯示資料
+		selectAll(con);
+		
+		//刪除 Table
+		dropTable(con);
+		
+		//關閉資料庫, 斷線
+		con.close();
+	}
+	
+	public Connection getConnection() throws SQLException
+	{
+		SQLiteConfig config = new SQLiteConfig();
+		
+		//config.setReadOnly(true);
+		config.setSharedCache(true);
+		config.enableRecursiveTriggers(true);
+		
+		SQLiteDataSource ds = new SQLiteDataSource();
+		ds.setUrl(DBFILE);
+		return ds.getConnection();
+		//ds.setServerNAme("");
+	}
+	
+	//Create Table
+	public void createTable(Connection con) throws SQLException
+	{
+		String sql = "DROP TABLE IF EXISTS test ; create table test (id integer, name string); ";
+		Statement stat = null;
+		stat = con.createStatement();
+		stat.executeUpdate(sql);
+	}
+	
+	//drop table
+	public void dropTable(Connection con) throws SQLException
+	{
+		String sql = "drop table test";
+		Statement stat = null;
+		stat = con.createStatement();
+		stat.executeUpdate(sql);
+	}
+	
+	//insert
+	public void insert(Connection con, int id, String name) throws SQLException
+	{
+		String sql = "insert into test (id,name) values(?,?)";
+		PreparedStatement pst = null;
+		pst = con.prepareStatement(sql);
+		int idx = 1;
+		pst.setInt(idx++, id);
+		pst.setString(idx++, name);
+		pst.executeUpdate();
+	}
+	
+	//update
+	public void update(Connection con, int id, String name) throws SQLException
+	{
+		String sql = "update test set name = ? where id = ?";
+		PreparedStatement pst = null;
+		pst = con.prepareStatement(sql);
+		int idx = 1;
+		pst.setString(idx++, name);
+		pst.setInt(idx++, id);
+		pst.executeUpdate();
+	}
+	
+	//delete
+	public void delete(Connection con, int id) throws SQLException
+	{
+		String sql = "delete from test where id = ?";
+		PreparedStatement pst = null;
+		pst = con.prepareStatement(sql);
+		int idx = 1;
+		pst.setInt(idx++, id);
+		pst.executeUpdate();
+	}
+	
+	//select All
+	public void selectAll(Connection con) throws SQLException
+	{
+		String sql = "select * from test";
+		Statement stat = null;
+		ResultSet rs = null;
+		stat = con.createStatement();
+		rs = stat.executeQuery(sql);
+		while(rs.next())
+		{
+			System.out.println(rs.getInt("id") + "\t" + rs.getString("name"));
+		}
+	}
+	
+}
---
+++ src/main.java
@@ -0,0 +1,28 @@
+import java.sql.SQLException;
+
+public class main 
+{
+	public main() {}
+	
+	public static void testJDBC()
+	{
+		try 
+		{
+			SQLiteJDBC test = new SQLiteJDBC();
+		} 
+		catch (SQLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("testJDBC(), in main.");
+	}
+	
+	public static void main(String[] args)
+	{
+		testJDBC();
+		
+		System.out.println("Hello Java for JDBC of SQLite.");
+	}
+
+}
