94b124c4b9ee2e9d271e17b694fa7b97059ebb70
tomcat <risc604@ms40.url.com.tw>
2017-01-31 00:52:46 +0800

    add Utils.java for parser raw data & calculate date time for after 3 hours.

--- src/ParserData.java
+++ src/ParserData.java
@@ -1,5 +1,8 @@
 import java.sql.SQLException;
+import java.text.Format;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class ParserData
@@ -18,6 +21,93 @@ public class ParserData
 		tempList = sqlObj.getTempList(sqlObj.getConnection(), userList.get(1).others[3]);
 		tableInfo(tempList);	//debug message
 		
+		int idx = compareDateTime(tempList);
+		
+		List<byte[]> tempData = getDataList(tempList, idx);
+	}
+	
+	public List<byte[]> getDataList(List<FieldObj> srcData, int indx)
+	{
+		List<byte[]> byteList = new ArrayList<>();
+		
+		for(int i=0; i<indx; i++)
+		{
+			//String tmpData = srcData.get(i).others[1];
+			byte[] dataByte = Utils.hexStringToByteArray(srcData.get(i).others[1]);
+			System.out.printf("%ndataByet length: %02d ", dataByte.length/3);
+			
+			for(int j=0; j<dataByte.length; j+=3)
+			{
+				byte[] rawData = new byte[3];
+				rawData[0] = dataByte[j];
+				rawData[1] = dataByte[j+1];
+				rawData[2] = dataByte[j+2];
+				byteList.add(rawData);
+				System.out.printf(", [%02d]:%02d, [%02d]:%02d, [%02d]:%02d", 
+						0, rawData[0], 1, rawData[1], 2, rawData[2] );
+			}
+		}
+		
+		//debug message.
+		for(int i=0; i<byteList.size(); i++)
+		{
+			//for(int j=0; j<3; j++)
+			{
+				System.out.printf("%n[%02d]: %s ", i, Utils.getHexToString(byteList.get(i)) );
+			}
+		}
+		
+		
+		System.out.printf("%n byteList size: %02d %n", byteList.size());
+		return byteList;
+	}
+	
+	public int compareDateTime(List<FieldObj> fieldObj)
+	{
+		int tempIdx = 0;
+		Date firstDT = StringToDate(fieldObj.get(0).others[0]);
+		System.out.println("compareDateTime(), firstDT: " + firstDT );
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(firstDT);
+		cal.add(cal.MINUTE, 180);
+		System.out.println("+180min: " + cal.getTime() );
+		
+		List<Date> endTimeList = new ArrayList<>();
+		for (int i=0; i<fieldObj.size(); i++) 
+		{
+			Date tmpET = StringToDate(fieldObj.get(i).others[2]);
+			System.out.printf(", [%02d]tmpET:%s %n", i, tmpET);
+			
+			if(tmpET.compareTo(cal.getTime()) <= 0)
+				endTimeList.add(tmpET);
+			else
+			{
+				tempIdx = i;
+				System.out.println("i = " + i);
+				break;
+			}
+		}
+		//System.out.printf(", args)
+		return tempIdx;
+	}
+	
+	public Date StringToDate(String srcTime)
+	{
+		//Date tmpDate = new Date();
+		byte[]	startTime = Utils.hexStringToByteArray(srcTime);
+		int[]	intTmp = new int[5]; 
+				intTmp[0] = Utils.byteToUnsignedInt(startTime[0]) + 2000;
+				intTmp[1] = Utils.byteToUnsignedInt(startTime[1]);
+				intTmp[2] = Utils.byteToUnsignedInt(startTime[2]);
+				intTmp[3] = Utils.byteToUnsignedInt(startTime[3]);
+				intTmp[4] = Utils.byteToUnsignedInt(startTime[4]);
+				//intTmp[5] = Utils.byteToUnsignedInt(startTime[5]);
+				
+		String tmpDT = String.format("%04d/%02d/%02d %02d:%02d:%02d", 
+				intTmp[0], intTmp[1]+1, intTmp[2], intTmp[3], intTmp[4], 0);
+		
+		System.out.printf("StringToDate(), tmpDT: %s", tmpDT);
+		return(new Date(tmpDT));
 	}
 	
 	private void tableInfo(List<FieldObj> tabList)
--- src/Utils.java
+++ src/Utils.java
@@ -1,4 +1,4 @@
-package src;
+//package ;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
